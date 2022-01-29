f4eb60a5129805c2abb01d56ef614f72707b0810
jakobottar <jakobottar@pm.me>
2022-01-29 14:55:14 -0700

    added echo command

--- README.md
+++ README.md
@@ -6,7 +6,8 @@ Edit `config.json` to include your secret token.
 ## ToDo List
 
 * ~~Mention sender in "pong" response~~
-* !echo command
+  * Add (real) ping stats to ping command
+* ~~!echo command~~
 * Voice channel creator
 * Role manager with reactions (like carl)
 * Timer
--- bot/bot.go
+++ bot/bot.go
@@ -3,6 +3,7 @@ package bot
 import (
 	"ava-go/config"
 	"fmt"
+	"strings"
 
 	"github.com/bwmarrin/discordgo"
 )
@@ -45,6 +46,32 @@ func messageHandler(s *discordgo.Session, m *discordgo.MessageCreate) {
 	// ping command - check for life
 	if m.Content == "ping" {
 		// mention the sender back and say pong!
-		_, _ = s.ChannelMessageSend(m.ChannelID, m.Author.Mention()+", pong!")
+		_, err := s.ChannelMessageSend(m.ChannelID, m.Author.Mention()+", pong!")
+		if err != nil {
+			fmt.Println(err.Error())
+		}
+	}
+
+	// if the first character of the message matches our bot prefix
+	if m.Content[0:1] == config.BotPrefix {
+		words := strings.Fields(m.Content) // split message string on space
+		command := words[0][1:]            // get first word and remove BotPrefix
+		args := words[1:]                  // get the rest of the message
+
+		switch command { // switch on command word
+		case "echo": // echo the message that was sent
+			echo(s, m, args)
+
+		default: // if the command does not match an existing one, return
+			return
+		}
+	}
+}
+
+func echo(s *discordgo.Session, m *discordgo.MessageCreate, args []string) {
+	// return a message back to the channel, echoing whatever is in the args
+	_, err := s.ChannelMessageSend(m.ChannelID, strings.Join(args, " "))
+	if err != nil {
+		fmt.Println(err.Error())
 	}
 }
