472bb9d76785e79a695482ce48a64f045cbb964a
Devyn Cyphers <devyncyphers@gmail.com>
2022-05-18 20:47:45 -0600

    Create dockerfile

--- main.go
+++ main.go
@@ -4,19 +4,10 @@ import (
 	"ava-go/bot"
 	"ava-go/config"
 	"log"
-	"os"
 )
 
 func main() {
-	f, err := os.OpenFile("bot.log", os.O_WRONLY|os.O_CREATE|os.O_APPEND, 0644)
-	if err != nil {
-		log.Fatal(err)
-		return
-	}
-	defer f.Close()
-	log.SetOutput(f)
-
-	err = config.ReadConfig()
+	err := config.ReadConfig()
 	if err != nil {
 		log.Println(err.Error())
 		return
