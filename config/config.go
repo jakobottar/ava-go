f9ac66749c9b1b35f643828d3c65d9ca004a6d83
jakobottar <jakobottar@pm.me>
2022-06-10 16:45:31 -0600

    added nicer error messages and updated dependencies

--- config/config.go
+++ config/config.go
@@ -21,14 +21,14 @@ func ReadConfig() error {
 	log.Println("config: reading config file...")
 	file, err := ioutil.ReadFile("./config.json")
 	if err != nil {
-		log.Fatalln(err.Error())
+		log.Fatalln("\u001b[31mERROR:\u001b[0m", err.Error())
 		return err
 	}
 
 	// log.Println(string(file))
 	err = json.Unmarshal(file, &config)
 	if err != nil {
-		log.Fatalln(err.Error())
+		log.Fatalln("\u001b[31mERROR:\u001b[0m", err.Error())
 		return err
 	}
 
