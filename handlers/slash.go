8fbc3c8d80b6c62e0ef26a4339de96e43ac286d9
jakobottar <jakobottar@pm.me>
2022-08-24 13:55:51 -0600

    ✨ add glizzy to slashcommands

--- handlers/slash.go
+++ handlers/slash.go
@@ -13,6 +13,18 @@ var (
 			Name:        "ping",
 			Description: "check if the bot is running",
 		},
+		{
+			Name:        "glizzy",
+			Description: "glizzy gladiators",
+			Options: []*discordgo.ApplicationCommandOption{
+				{
+					Type:        discordgo.ApplicationCommandOptionString,
+					Name:        "content",
+					Description: "emote or content",
+					Required:    true,
+				},
+			},
+		},
 	}
 
 	CommandHandlers = map[string]func(session *discordgo.Session, interaction *discordgo.InteractionCreate){
@@ -25,5 +37,37 @@ var (
 				},
 			})
 		},
+		"glizzy": func(session *discordgo.Session, interaction *discordgo.InteractionCreate) {
+			log.Println("slashhandler: caught glizzy command")
+
+			// get map of options
+			optionMap := mapOptions(interaction)
+
+			// TODO: I don't think I can not respond, but at least I can hide the response
+			session.InteractionRespond(interaction.Interaction, &discordgo.InteractionResponse{
+				Type: discordgo.InteractionResponseChannelMessageWithSource,
+				Data: &discordgo.InteractionResponseData{
+					Content: "🌭",
+					Flags:   discordgo.MessageFlagsEphemeral,
+				},
+			})
+
+			msg := "<a:glizzyR:991176701063221338>" + optionMap["content"].StringValue() + "<a:glizzyL:991176582402150531>"
+			if _, err := session.ChannelMessageSend(interaction.ChannelID, msg); err != nil {
+				log.Println("\u001b[31mERROR:\u001b[0m", err.Error())
+				return
+			}
+		},
 	}
 )
+
+func mapOptions(interaction *discordgo.InteractionCreate) (optionMap map[string]*discordgo.ApplicationCommandInteractionDataOption) {
+	// get map of options
+	options := interaction.ApplicationCommandData().Options
+	optionMap = make(map[string]*discordgo.ApplicationCommandInteractionDataOption, len(options))
+	for _, opt := range options {
+		optionMap[opt.Name] = opt
+	}
+
+	return
+}
